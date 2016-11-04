ce2e3d1af1e4ad9db631bbf356889f824a65773c
Steph Burton <stephanie@raspberrypi.org>
2016-11-04 11:09:57 +0000

    test

---
+++ test turtle.py
@@ -0,0 +1,11 @@
+Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
+[GCC 4.9.1] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
